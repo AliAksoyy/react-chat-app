b94b6e50903be3e65e395441236fac8d2f8183aa
AliAksoyy <alifeyza4017@gmail.com>
2024-03-17 01:22:26 +0300

    aaadded

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,6 +8,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, //!prevent Xss attacks cross-site scripting attacks
     sameSite: "strict", //? CSRF attacks cross-site forgery attacks
+    secure: process.env.NODE_ENV !== "development",
   });
 };
 
