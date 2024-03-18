3cb618f9986deba8aafb1f91228c246187933888
AliAksoyy <alifeyza4017@gmail.com>
2024-03-18 11:00:34 +0300

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import "./index.css";
 const App = () => {
   return (
     <>
-    <div className="border bottom-1 bg-slate-400 accent-slate-200">a</div>
       <button className="btn btn-primary">One</button>
       <button className="btn btn-secondary">Two</button>
       <button className="btn btn-accent btn-outline">Three</button>
