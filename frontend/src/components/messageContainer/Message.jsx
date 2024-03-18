e292048f8fd73796fb41c2bf59e673104cde7e39
AliAksoyy <alifeyza4017@gmail.com>
2024-03-18 14:23:46 +0300

    added

--- frontend/src/components/messageContainer/Message.jsx
+++ frontend/src/components/messageContainer/Message.jsx
@@ -1,19 +1,23 @@
 const Message = () => {
   return (
-    <div className=" chat chat-end">
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS chat bubble component"
-            src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-          />
+    <>
+      <div className=" chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
+          </div>
+        </div>
+        <div className="chat-bubble text-white bg-blue-500">
+          Hi What is ups?
+        </div>
+        <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-gray-100 pt-1">
+          12:42
         </div>
       </div>
-      <div className="chat-bubble text-white bg-blue-500">Hi What is ups?</div>
-      <div className="chat-footer opacity-50 text-xs flex gap-1 items-center text-gray-100 pt-1">
-        12:42
-      </div>
-      {/* <div className="chat chat-start">
+      <div className="chat chat-start">
         <div className="chat-image avatar">
           <div className="w-10 rounded-full">
             <img
@@ -29,23 +33,7 @@ const Message = () => {
         <div className="chat-bubble">You were the Chosen One!</div>
         <div className="chat-footer opacity-50">Delivered</div>
       </div>
-      <div className="chat chat-end">
-        <div className="chat-image avatar">
-          <div className="w-10 rounded-full">
-            <img
-              alt="Tailwind CSS chat bubble component"
-              src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-            />
-          </div>
-        </div>
-        <div className="chat-header">
-          Anakin
-          <time className="text-xs opacity-50">12:46</time>
-        </div>
-        <div className="chat-bubble">I hate you!</div>
-        <div className="chat-footer opacity-50">Seen at 12:46</div>
-      </div> */}
-    </div>
+    </>
   );
 };
 
