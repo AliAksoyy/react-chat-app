6a5b46d6cc3bf6a725ef29c6273368b88e8ff97b
AliAksoyy <alifeyza4017@gmail.com>
2024-03-20 13:32:24 +0300

    add

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,28 +1,7 @@
-import { useEffect, useState } from "react";
 import MessageContainer from "../../components/messageContainer/MessageContainer";
 import SideBar from "../../components/sideBar/SideBar";
 
 const Home = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    async function getData() {
-      try {
-        const res = await fetch("http://localhost:8000/api/users", {
-          credentials: "include",
-          headers: {
-            Authorization: `Bearer ${"jwtToken"}`,
-          },
-        });
-        const data = await res.json();
-        console.log("data", data);
-        setData(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    getData();
-  }, []);
   return (
     <div className=" flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-0">
       <SideBar />
