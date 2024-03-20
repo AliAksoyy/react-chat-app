98b007ffbc833de54c03b987d61ad816aeb67a9b
AliAksoyy <alifeyza4017@gmail.com>
2024-03-20 13:41:33 +0300

    added

--- frontend/src/components/sideBar/Conversations.jsx
+++ frontend/src/components/sideBar/Conversations.jsx
@@ -1,6 +1,10 @@
 import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
 
 const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+  console.log(conversations);
+
   return (
     <div className="flex flex-col py-2">
       <Conversation />
