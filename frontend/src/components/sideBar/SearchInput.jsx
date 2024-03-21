0d54abd9b8dfccf438a5312b7a9b216e1af1ff22
AliAksoyy <alifeyza4017@gmail.com>
2024-03-21 23:37:38 +0300

    added

--- frontend/src/components/sideBar/SearchInput.jsx
+++ frontend/src/components/sideBar/SearchInput.jsx
@@ -1,11 +1,42 @@
+import { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
+import useGetConversations from "../../hooks/useGetConversations";
+import useConversation from "../../zustand/useConversation";
+import toast from "react-hot-toast";
 const SearchInput = () => {
+  const [searchName, setSearchName] = useState("");
+
+  const { conversations } = useGetConversations();
+  const { setSelectedConversation } = useConversation();
+
+  const handlerFilter = (e) => {
+    e.preventDefault();
+    if (!searchName) return;
+    if (searchName.length < 3) {
+      return toast.error("Search term must be at least 3 characters");
+    }
+    const conversation = conversations?.filter((conversation) => {
+      return conversation.fullName
+        .toLowerCase()
+        .includes(searchName.toLowerCase());
+    });
+    console.log(conversation);
+    if (conversation?.length > 0) {
+      setSelectedConversation(conversation[0]);
+      setSearchName("");
+    } else {
+      return toast.error("No such user found");
+    }
+  };
+
   return (
-    <form className="flex items-center gap-2">
+    <form onSubmit={handlerFilter} className="flex items-center gap-2">
       <input
         type="text"
         placeholder="Search..."
         className="input input-bordered rounded-full bg-gray-900 text-gray-300"
+        value={searchName}
+        onChange={(e) => setSearchName(e.target.value)}
       />
       <button
         type="submit"
